e0da286a2d511b4113ca55013a30e82e9d35b1b0
Nick Dunn <nickdunn2@gmail.com>
2016-03-03 18:27:27 -0600

    make models & migrations

---
+++ database/migrations/2016_03_04_002516_create_tag_task_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tag_task', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tag_id')->unsigned()->index();
+            $table->integer('task_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('tag_task');
+    }
+}
